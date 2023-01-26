e4e16c9d430bf622eaeb5a6fc60d771c4d3c3888
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-01-26 21:32:32 +0530

    init

---
+++ src/main/java/com/aspect/aspectprogramming/AspectprogrammingApplication.java
@@ -0,0 +1,13 @@
+package com.aspect.aspectprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectprogrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectprogrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspect/aspectprogramming/AspectprogrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspect.aspectprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectprogrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
